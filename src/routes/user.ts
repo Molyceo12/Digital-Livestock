2c0f4e27e3657398a4fc17b19d1ecd7dad438717
Bertin <bizimanabertin2002@gmail.com>
2025-09-12 14:25:20 +0200

    my sign up

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -1,13 +1,13 @@
 import express from "express";
-import { registerUser } from "../controllers/auth/register";
-import { loginUser } from "../controllers/auth/login";
+// import { registerUser } from "../controllers/auth/register";
+// import { loginUser } from "../controllers/auth/login";
 import { signupUser } from "../controllers/auth/signup";
 import { signinUser } from "../controllers/auth/signin";
 const router = express.Router();
 
 
-router.post("api/register", registerUser);
-router.post("api/login", loginUser);
+// router.post("api/register", registerUser);
+// router.post("api/login", loginUser);
 router.post("/api/signup", signupUser); 
 router.post("/api/signin", signinUser); 
 
